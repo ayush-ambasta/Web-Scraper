3515f1d385f63cc165eaa1d3dd9c0c9c7e508f9b
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-01-01 14:24:29 +0530

    database setup

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -2,14 +2,14 @@
 
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { extractPrice } from '../utils';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
   
     // BrightData proxy configuration
-    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const username = String(process.env.                                                                                                                                                                                                                                                 HT_DATA_USERNAME);
     const password = String(process.env.BRIGHT_DATA_PASSWORD);
     const port = 22225;
     const session_id = (1000000 * Math.random()) | 0;
@@ -43,7 +43,33 @@ export async function scrapeAmazonProduct(url: string) {
             $('#priceblock_dealprice'),
             $('.a-size-base.a-color-price')
           );
-          const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+        const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+        const images = 
+        $('#imgBlkFront').attr('data-a-dynamic-image') || 
+        $('#landingImage').attr('data-a-dynamic-image') ||
+        '{}'
+
+        const imageUrls = Object.keys(JSON.parse(images));
+        const currency = extractCurrency($('.a-price-symbol'));
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+        const description = extractDescription($)
+        
+        // Construct data object with scraped information
+        const data = {
+            url,
+            currency: currency || '$',
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            averagePrice: Number(currentPrice) || Number(originalPrice),
+        }
     }catch(error:any){
         throw new Error(`Failed to scrape product: ${error.message}`);
     }
